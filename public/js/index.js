d5c724647d7afd0097ed1faff394ebea46fda3f0
Johnathon Hutt <johnathonhutt@gmail.com>
2019-09-18 16:37:46 -0400

    second commit pre refactoring

--- public/js/index.js
+++ public/js/index.js
@@ -9,15 +9,19 @@ shipSprite.src = "images/ship64.png";
 const alienImage = new Image();
 alienImage.src = "images/alien.png";
 
+const alienRedImage = new Image();
+alienRedImage.src = "images/alienRedWithExplosion.png";
+
 const ship = {
   x: canvas.width / 2 - 32,
   y: canvas.height - 70
 };
 
 const lasers = [];
+let aliens = [];
 
 let counter = 0;
-let frameRate = 60/4;
+let frameRate = 60 / 4;
 let frame = 1;
 
 const scale = 1;
@@ -31,19 +35,6 @@ document.addEventListener('keyup', keyUpHandler, false);
 
 let rPressed = false;
 let lPressed = false;
-
-const brick = {
-  colCount: 8,
-  rowCount: 4,
-  h: 12,
-  w: 35,
-  p: 12,
-  offSetTop: 12,
-  offSetLeft: 18,
-  moveCount: 0,
-  dir: "r",
-  dropSpeed: 4
-};
 
 // function ShieldObjGenerator() {
 //   this.h = 10;
@@ -106,19 +97,6 @@ function keyDownHandler(e) {
       break;
     default:
       console.log(e.keyCode);
-  }
-}
-
-let bricks = [];
-for (let c = 0; c < brick.colCount; c++) {
-  bricks[c] = [];
-  for (let r = 0; r < brick.rowCount; r++) {
-    bricks[c][r] = {
-      x: 0,
-      y: 0,
-      color: "white",
-      status: 2
-    };
   }
 }
 
@@ -191,30 +169,83 @@ function moveLasers() {
 //       alien.offSetLeft -= 4;
 //       if (alien.offSetLeft === 6) {
 //         alien.dir = 'r';
-//         alien.offSetTop += brick.dropSpeed;
+//         alien.offSetTop += alien.dropSpeed;
 //       }
 //     }
 //   }
 // }
 
-function moveBricks() {
-  brick.moveCount += 1;
-  if (brick.moveCount % 10 === 0) {
-    if (brick.dir === 'r') {
-      brick.offSetLeft += 4;
-      if (brick.offSetLeft === 26) {
-        brick.dir = 'l';
-        brick.offSetTop += brick.dropSpeed;
-      }
-    } else if (brick.dir === 'l') {
-      brick.offSetLeft -= 4;
-      if (brick.offSetLeft === 6) {
-        brick.dir = 'r';
-        brick.offSetTop += brick.dropSpeed;
-      }
+//figure out how to make them look for each other
+//or just get them to not bunch up
+// function moveAliens() {
+//   //for 2D artray - has issues
+//   alien.moveCount += 1;
+//   if (alien.moveCount % 10 === 0) {
+//     //move every 10 frames
+//     for (let i = 0; i < aliens.length; i++) {
+//       //check if last alien in row is near the edge - switch directions
+//       if (aliens[i][aliens[i].length - 1].x + 36 >= canvas.width || aliens[i][0].x - 4 <= 0) {
+//         for (let j = 0; j < aliens[i].length; j++) {
+//           (aliens[i][j].dir === 'r') ? aliens[i][j].dir = 'l': aliens[i][j].dir = 'r';
+//           //add appropriate x +/- and add dropspeed to y
+//           (aliens[i][j].dir === 'r') ? aliens[i][j].x += aliens[i][j].speedX: aliens[i][j].x -= aliens[i][j].speedX;
+//         }
+//       } else {
+//         for (let k = 0; k < aliens[i].length; k++) {
+//           (aliens[i][k].dir === 'r') ? aliens[i][k].x += aliens[i][k].speedX: aliens[i][k].x -= aliens[i][k].speedX;
+//         }
+//       }
+//     }
+//   }
+// }
+
+// function moveAliens() {
+//   alien.moveCount += 1;
+//   if (alien.moveCount % 10 === 0) {
+//     //move every 10 frames
+//     for (let i = 0; i < aliens.length; i++) {
+//       //check if last alien in row is near the edge - switch directions
+//       if (aliens[i].x + 36 >= canvas.width || aliens[i].x - 4 <= 0) {
+//         for (let j = 0; j < aliens.length; j++) {
+//           (aliens[j].dir === 'r') ? aliens[j].dir = 'l': aliens[j].dir = 'r';
+//           //add appropriate x +/- and add dropspeed to y
+//           (aliens[j].dir === 'r') ? aliens[j].x += aliens[j].speedX: aliens[j].x -= aliens[j].speedX;
+//         }
+//         break;
+//       } else {
+//         for (let k = 0; k < aliens[i].length; k++) {
+//           (aliens[k].dir === 'r') ? aliens[k].x += aliens[i][k].speedX: aliens[i][k].x -= aliens[i][k].speedX;
+//         }
+//       }
+//     }
+//   }
+// }
+
+function moveAliens() {
+  let nearAnEdge = false;
+  for (let i = 0; i < aliens.length; i++) {
+    //iterate through aliens array check if any are near edge
+    if (aliens[i].x + 36 >= canvas.width || aliens[i].x - 4 <= 0) {
+      nearAnEdge = true;
+      break;
+    }
+  }
+  //if near edge switch dir + move, else just move
+  if (nearAnEdge) {
+    for (let j = 0; j < aliens.length; j++) {
+      (aliens[j].dir === 'r') ? aliens[j].dir = 'l': aliens[j].dir = 'r';
+      //add appropriate x +/- and add dropspeed to y
+      (aliens[j].dir === 'r') ? aliens[j].x += aliens[j].speedX: aliens[j].x -= aliens[j].speedX;
+      aliens[j].y += aliens[j].speedY;
+    }
+  } else {
+    for (let k = 0; k < aliens.length; k++) {
+      (aliens[k].dir === 'r') ? aliens[k].x += aliens[k].speedX: aliens[k].x -= aliens[k].speedX;
     }
   }
 }
+
+
 
 function drawLasers() {
   moveLasers();
@@ -227,150 +258,166 @@ function drawLasers() {
   }
 }
 
-function drawBricks() {
-  for (let c = 0; c < brick.colCount; c++) {
-    for (let r = 0; r < brick.rowCount; r++) {
-      let brickX = (c * (brick.w + brick.p) + brick.offSetLeft);
-      let brickY = (r * (brick.h + brick.p) + brick.offSetTop);
-      if (bricks[c][r].status >= 1) {
-        bricks[c][r].x = brickX;
-        bricks[c][r].y = brickY;
-        ctx.beginPath();
-        ctx.rect(brickX, brickY, brick.w, brick.h);
-        ctx.fillStyle = bricks[c][r].color;
-        ctx.fill();
-        ctx.closePath();
-      }
-    }
-  }
+
+function drawAlien(frame, canvasX, canvasY) {
+  ctx.drawImage(alienImage, frame * w, 0, w, h, canvasX, canvasY, scaledW, scaledH);
 }
 
-// const lShield = makeNewShield(46, 490);
-// const mShield = makeNewShield(164, 490);
-// const rShield = makeNewShield(282, 490);
-//
-// function drawShield(shield) {
-//   for (let c = 0; c < shield[0][0].colNumber; c++) {
-//     for (let r = 0; r < shield[0][0].rowNumber; r++) {
-//       let s = shield[c][r];
-//       if (s.status > 0) {
-//         ctx.beginPath();
-//         ctx.rect(s.x, s.y, s.w, s.h);
-//         ctx.fillStyle = s.color;
-//         ctx.fill();
-//         ctx.closePath();
-//       }
-//     }
-//   }
-// }
+function drawRedAlien(frame, canvasX, canvasY) {
+  ctx.drawImage(alienRedImage, frame * w, 0, w, h, canvasX, canvasY, scaledW, scaledH);
+}
 
-
-
-function drawFrame(frame, canvasX, canvasY) {
-    ctx.drawImage(alienImage, frame*w, 0, w, h, canvasX, canvasY, scaledW, scaledH);
+function drawExplosion(frame, canvasX, canvasY) {
+  //need to work in timing
+  ctx.drawImage(alienRedImage, frame * w, 32, w, h, canvasX, canvasY, scaledW, scaledH);
 }
 
 const alien = {
   colCount: 8,
   rowCount: 4,
+  totAliens: 8 * 4,
   h: 12,
-  w: 35,
+  w: 32,
   pTop: 18,
   pSides: 6,
   offSetTop: 12,
   offSetLeft: 18,
-  moveCount: 0,
-  dir: "r",
-  dropSpeed: 4
+  // moveCount: 0,
 };
 
-let aliens = [];
-for (let c = 0; c < alien.colCount; c++) {
-  aliens[c] = [];
-  for (let r = 0; r < alien.rowCount; r++) {
-    aliens[c][r] = {
-      x: 0,
-      y: 0,
-      status: 2
-    };
-  }
-}
 
-function drawAliens() {
-  for (let c = 0; c < alien.colCount; c++) {
-    for (let r = 0; r < alien.rowCount; r++) {
-      let alienX = (c * (alien.w + alien.pSides) + alien.offSetLeft);
-      let alienY = (r * (alien.h + alien.pTop) + alien.offSetTop);
-      if (aliens[c][r].status >= 1) {
-        aliens[c][r].x = alienX;
-        aliens[c][r].y = alienY;
-        drawFrame(frame, alienX, alienY);
-      }
+// for (let c = 0; c < alien.colCount; c++) {
+//   aliens[c] = [];
+//   for (let r = 0; r < alien.rowCount; r++) {
+//     aliens[c][r] = {
+//       x: 0,
+//       y: 0,
+//       status: 2
+//     };
+//   }
+// }
+
+// function populateAlienArr() {
+//   for (let c = 0; c < alien.colCount; c++) {
+//     aliens[c] = [];
+//     for (let r = 0; r < alien.rowCount; r++) {
+//       let alienX = (c * (alien.w + alien.pSides) + alien.offSetLeft);
+//       let alienY = (r * (alien.h + alien.pTop) + alien.offSetTop);
+//       aliens[c].push({
+//         x: alienX,
+//         y: alienY,
+//         dir: 'r',
+//         speedY: 4,
+//         speedX: 4,
+//         status: 2
+//       });
+//     }
+//   }
+// }
+
+function populateAlienArr() {
+  //1D array version
+  //accesses global aliens array
+  let alienX = alien.offSetLeft;
+  let alienY = alien.offSetTop;
+  for (let i = 0; i < alien.totAliens; i++) {
+    aliens.push({
+      x: alienX,
+      y: alienY,
+      status: 2,
+      speedX: 1,
+      speedY: 4,
+      dir: 'r',
+    });
+    if ((i + 1) % alien.colCount === 0) {
+      alienX = alien.offSetLeft;
+      alienY += (alien.h + alien.pTop);
+    } else {
+      alienX += (alien.w + alien.pSides);
     }
   }
 }
 
+populateAlienArr();
+
+function drawAliens() {
+  //for 1D array version
+  for (let i = 0; i < aliens.length; i++) {
+    if (aliens[i].status === 2) {
+      drawAlien(frame, aliens[i].x, aliens[i].y);
+    } else if (aliens[i].status === 1) {
+      drawRedAlien(frame, aliens[i].x, aliens[i].y);
+    }
+  }
+}
+
+// function drawAliens() {
+//   for (let i = 0; i < aliens.length; i++) {
+//     for (let j = 0; j < aliens[0].length; j++) {
+//       drawFrame(frame, aliens[i][j].x, aliens[i][j].y);
+//     }
+//   }
+// }
+
+// function drawAliens() {
+//   for (let c = 0; c < alien.colCount; c++) {
+//     for (let r = 0; r < alien.rowCount; r++) {
+//       let alienX = (c * (alien.w + alien.pSides) + alien.offSetLeft);
+//       let alienY = (r * (alien.h + alien.pTop) + alien.offSetTop);
+//       if (aliens[c][r].status >= 1) {
+//         aliens[c][r].x = alienX;
+//         aliens[c][r].y = alienY;
+//         drawFrame(frame, alienX, alienY);
+//       }
+//     }
+//   }
+// }
+
+// function collisionDetection() {
+//   //brick collisions
+//   for (let c = 0; c < brick.colCount; c++) {
+//     for (let r = 0; r < brick.rowCount; r++) {
+//       let b = bricks[c][r];
+//       for (let i = 0; i < lasers.length; i++) {
+//         if (b.status > 0 && lasers[i].y >= b.y && lasers[i].y <= b.y + brick.h && lasers[i].x+4 >= b.x && lasers[i].x <= b.x + brick.w) {
+//           if (b.status === 2) {
+//             lasers.splice(i, 1);
+//             b.status--;
+//             b.color = "red";
+//             // console.log(b);
+//           } else if (b.status === 1) {
+//             lasers.splice(i, 1);
+//             b.status--;
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+
 function collisionDetection() {
-  //brick collisions
-  for (let c = 0; c < brick.colCount; c++) {
-    for (let r = 0; r < brick.rowCount; r++) {
-      let b = bricks[c][r];
-      for (let i = 0; i < lasers.length; i++) {
-        if (b.status > 0 && lasers[i].y >= b.y && lasers[i].y <= b.y + brick.h && lasers[i].x+4 >= b.x && lasers[i].x <= b.x + brick.w) {
-          if (b.status === 2) {
+  for (let i = 0; i < lasers.length; i++) {
+    let l = lasers[i];
+    //only check if lasers are >= height of lowest enemy
+    if (l.y <= aliens[aliens.length-1].y) {
+      console.log('At height');
+      for (let j = 0; j < aliens.length; j++) {
+        if (aliens[j].status > 0 && lasers[i].y >= aliens[j].y && lasers[i].y <= aliens[j].y + alien.h && lasers[i].x+4 >= aliens[j].x && lasers[i].x <= aliens[j].x + alien.w) {
+          console.log('It collided');
+          if (aliens[j].status === 2) {
             lasers.splice(i, 1);
-            b.status--;
-            b.color = "red";
-            // console.log(b);
-          } else if (b.status === 1) {
+            aliens[j].status--;
+            //change animation/color
+            break;
+          } else if (aliens[j].status === 1) {
             lasers.splice(i, 1);
-            b.status--;
+            aliens[j].status--;
+            break;
           }
         }
       }
     }
   }
-  // console.log('running collision detection');
-  // for (let c = 0; c < lShield.colCount; c++) {
-  //   for (let r = 0; r < lShield.rowCount; r++) {
-  //     let s = lShield[c][r];
-  //     console.log('three'); //not showing up
-  //     for (let j = 0; j < lasers.length; j++) {
-  //       if (s.status > 0 && lasers[j].x + lasers[j].width >= s.x && lasers[j].x <= s.x + s.w && lasers[j].y >= s.y && lasers[j].y <= s.y + s.h) {
-  //         console.log('got through');
-  //         //remove laser and shield component
-  //         lasers.splice(j, 1);
-  //         s.status--;
-  //       }
-  //     }
-  //   }
-  // }
-  //shield collisions --is the laser moving before it can complete the loop?
-  // for (let j = 0; j < lasers.length; j++) {
-  //
-  //   if (lasers[j].x + lasers[j].w >= lShield[0][0].x && lasers[j].x <= lShield[0][0].x + lShield[0][0].totW) {
-  //     for
-  // console.log('In box');
-  // for (let c = 0; c < lShield.length; c++) {
-  //   for (let r = 0; r < lShield.length; r++) {
-  //     let s = lShield[c][r];
-  //     console.log('s.x: ' + s.x);
-  //     console.log(lasers[j].y);
-  //     console.log('s.y + s.h:' + (s.y+s.h));
-  //     console.log(s.status > 0); //good
-  //     console.log(lasers[j].x + lasers[j].width >= s.x); //check
-  //     console.log(lasers[j].x <= s.x + s.w); //good --changed s.totW to s.w
-  //     console.log(lasers[j].y >= s.y);
-  //     console.log(lasers[j].y <= s.y + s.h); //check
-  //     if (s.status > 0 && lasers[j].x + lasers[j].width >= s.x && lasers[j].x <= s.x + s.totW && lasers[j].y >= s.y && lasers[j].y <= s.y + s.h) {
-  //       //remove laser and shield component
-  //       lasers.splice(j, 1);
-  //       s.status--;
-  //     }
-  //   }
-  // }
-  // }
-  // }
 }
 
 // function checkShieldCollision(shield, laser) {
@@ -390,13 +437,13 @@ function collisionDetection() {
 
 function draw() {
   counter += 1;
-  if (counter%frameRate === 0) {
-     if (frame === 9) {
-       frame = 0;
-     } else {
-       frame += 1;
-     }
-   }
+  if (counter % frameRate === 0) {
+    if (frame === 9) {
+      frame = 0;
+    } else {
+      frame += 1;
+    }
+  }
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawShip();
@@ -407,9 +454,9 @@ function draw() {
   checkR();
   drawLasers();
   collisionDetection();
-  moveBricks();
-  drawBricks();
-  // drawAliens();
+  // moveBricks();
+  moveAliens();
+  drawAliens();
   requestAnimationFrame(draw);
 }
 
